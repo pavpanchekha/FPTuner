8182a1f2821218911aa19dec336e903a95cec079
Ian Briggs <ianbriggsutah@gmail.com>
2020-09-18 17:48:58 +0000

    Check now has some benchmarks with sin/tan/log

--- bin/check.sh
+++ bin/check.sh
@@ -12,28 +12,37 @@ date=$(date +%s)
 
 FPTUNER="${SCRIPT_LOCATION}/fptuner --nightly --cfile"
 
-cat <<EOF > $date.html
+mkdir $date
+cat <<EOF > $date/index.html
 <!doctype html>
 <title>FPTuner Results for $(date +%Y-%m-%d)</title>
 <body>
 EOF
+${FPTUNER} ${SCRIPT_LOCATION}/../benchmarks/check_benchmarks/azimuth.fpcore -e 1e1 1e0 1e-1 1e-2 1e-3 1e-4 1e-5 1e-6 1e-7 1e-8 1e-9 1e-10 | tee -a $date/index.html
+${FPTUNER} ${SCRIPT_LOCATION}/../benchmarks/check_benchmarks/complex_sine_and_cosine.fpcore  -e 1e2 1e1 1e0 1e-1 1e-2 1e-3 1e-4 1e-5 1e-6 1e-7 1e-8 1e-9 1e-10 | tee -a $date/index.html
+${FPTUNER} ${SCRIPT_LOCATION}/../benchmarks/check_benchmarks/exp1x.fpcore  -e 1e2 1e1 1e0 1e-1 1e-2 1e-3 1e-4 1e-5 1e-6 1e-7 1e-8 1e-9 1e-10 1e-11 1e-12 1e-13 | tee -a $date/index.html
+${FPTUNER} ${SCRIPT_LOCATION}/../benchmarks/check_benchmarks/exp1x_log.fpcore  -e 1e2 1e1 1e0 1e-1 1e-2 1e-3 1e-4 1e-5 1e-6 1e-7 1e-8 1e-9 1e-10 1e-11 1e-12 1e-13 | tee -a $date/index.html
+${FPTUNER} ${SCRIPT_LOCATION}/../benchmarks/check_benchmarks/hartman3.fpcore -e 1e1 1e0 1e-1 1e-2 1e-3 1e-4 1e-5 1e-6 1e-7 1e-8 1e-9 1e-10 | tee -a $date/index.html
+${FPTUNER} ${SCRIPT_LOCATION}/../benchmarks/check_benchmarks/hartman6.fpcore -e 1e1 1e0 1e-1 1e-2 1e-3 1e-4 1e-5 1e-6 1e-7 1e-8 1e-9 1e-10 | tee -a $date/index.html
+${FPTUNER} ${SCRIPT_LOCATION}/../benchmarks/check_benchmarks/i6.fpcore -e 1e1 1e0 1e-1 1e-2 1e-3 1e-4 1e-5 1e-6 1e-7 1e-8 1e-9 1e-10 | tee -a $date/index.html
+${FPTUNER} ${SCRIPT_LOCATION}/../benchmarks/check_benchmarks/logexp2.fpcore  -e 1e2 1e1 1e0 1e-1 1e-2 1e-3 1e-4 1e-5 1e-6 1e-7 1e-8 1e-9 1e-10 1e-11 1e-12 1e-13 1e-14 | tee -a $date/index.html
+${FPTUNER} ${SCRIPT_LOCATION}/../benchmarks/check_benchmarks/logexp.fpcore  -e 1e2 1e1 1e0 1e-1 1e-2 1e-3 1e-4 1e-5 1e-6 1e-7 1e-8 1e-9 1e-10 1e-11 1e-12 1e-13 1e-14 | tee -a $date/index.html
+${FPTUNER} ${SCRIPT_LOCATION}/../benchmarks/check_benchmarks/nmse_example_3_7.fpcore  -e 1e2 1e1 1e0 1e-1 1e-2 1e-3 1e-4 1e-5 1e-6 1e-7 1e-8 1e-9 1e-10 | tee -a $date/index.html
+${FPTUNER} ${SCRIPT_LOCATION}/../benchmarks/check_benchmarks/nmse_problem_3_3_7.fpcore  -e 1e2 1e1 1e0 1e-1 1e-2 1e-3 1e-4 1e-5 1e-6 1e-7 1e-8 1e-9 1e-10 | tee -a $date/index.html
+${FPTUNER} ${SCRIPT_LOCATION}/../benchmarks/check_benchmarks/nmse_problem_3_4_2.fpcore  -e 1e2 1e1 1e0 1e-1 1e-2 1e-3 1e-4 1e-5 1e-6 1e-7 1e-8 | tee -a $date/index.html
+${FPTUNER} ${SCRIPT_LOCATION}/../benchmarks/check_benchmarks/nmse_problem_3_4_4.fpcore  -e 1e2 1e1 1e0 1e-1 1e-2 1e-3 1e-4 1e-5 1e-6 1e-7 1e-8 1e-9 1e-10 1e-11 1e-12 1e-13 | tee -a $date/index.html
+${FPTUNER} ${SCRIPT_LOCATION}/../benchmarks/check_benchmarks/nmse_section_3_11.fpcore  -e 1e2 1e1 1e0 1e-1 1e-2 1e-3 1e-4 1e-5 1e-6 1e-7 1e-8 1e-9 1e-10 1e-11 1e-12 1e-13 1e-14 | tee -a $date/index.html
+${FPTUNER} ${SCRIPT_LOCATION}/../benchmarks/check_benchmarks/sine.fpcore -e 1e1 1e0 1e-1 1e-2 1e-3 1e-4 1e-5 1e-6 1e-7 1e-8 1e-9 1e-10 | tee -a $date/index.html
+${FPTUNER} ${SCRIPT_LOCATION}/../benchmarks/check_benchmarks/sineOrder3.fpcore -e 1e1 1e0 1e-1 1e-2 1e-3 1e-4 1e-5 1e-6 1e-7 1e-8 1e-9 1e-10 | tee -a $date/index.html
+${FPTUNER} ${SCRIPT_LOCATION}/../benchmarks/check_benchmarks/sphere.fpcore -e 1e1 1e0 1e-1 1e-2 1e-3 1e-4 1e-5 1e-6 1e-7 1e-8 1e-9 1e-10 | tee -a $date/index.html
+${FPTUNER} ${SCRIPT_LOCATION}/../benchmarks/check_benchmarks/Statistics_Distribution_Poisson_Internal_probability_from_math_functions_0_1_5_2.fpcore  -e 1e2 1e1 1e0 1e-1 1e-2 1e-3 1e-4 1e-5 1e-6 1e-7 1e-8 1e-9 | tee -a $date/index.html
 
-${FPTUNER} ${SCRIPT_LOCATION}/../benchmarks/exp_benchmarks/complex_sine_and_cosine.fpcore  -e 1e2 1e1 1e0 1e-1 1e-2 1e-3 1e-4 1e-5 1e-6 1e-7 1e-8 1e-9 1e-10 | tee -a $date.html
-${FPTUNER} ${SCRIPT_LOCATION}/../benchmarks/exp_benchmarks/exp1x.fpcore  -e 1e2 1e1 1e0 1e-1 1e-2 1e-3 1e-4 1e-5 1e-6 1e-7 1e-8 1e-9 1e-10 1e-11 1e-12 1e-13 | tee -a $date.html
-${FPTUNER} ${SCRIPT_LOCATION}/../benchmarks/exp_benchmarks/exp1x_log.fpcore  -e 1e2 1e1 1e0 1e-1 1e-2 1e-3 1e-4 1e-5 1e-6 1e-7 1e-8 1e-9 1e-10 1e-11 1e-12 1e-13 | tee -a $date.html
-${FPTUNER} ${SCRIPT_LOCATION}/../benchmarks/exp_benchmarks/logexp2.fpcore  -e 1e2 1e1 1e0 1e-1 1e-2 1e-3 1e-4 1e-5 1e-6 1e-7 1e-8 1e-9 1e-10 1e-11 1e-12 1e-13 1e-14 | tee -a $date.html
-${FPTUNER} ${SCRIPT_LOCATION}/../benchmarks/exp_benchmarks/logexp.fpcore  -e 1e2 1e1 1e0 1e-1 1e-2 1e-3 1e-4 1e-5 1e-6 1e-7 1e-8 1e-9 1e-10 1e-11 1e-12 1e-13 1e-14 | tee -a $date.html
-${FPTUNER} ${SCRIPT_LOCATION}/../benchmarks/exp_benchmarks/nmse_example_3_7.fpcore  -e 1e2 1e1 1e0 1e-1 1e-2 1e-3 1e-4 1e-5 1e-6 1e-7 1e-8 1e-9 1e-10 | tee -a $date.html
-${FPTUNER} ${SCRIPT_LOCATION}/../benchmarks/exp_benchmarks/nmse_problem_3_3_7.fpcore  -e 1e2 1e1 1e0 1e-1 1e-2 1e-3 1e-4 1e-5 1e-6 1e-7 1e-8 1e-9 1e-10 | tee -a $date.html
-${FPTUNER} ${SCRIPT_LOCATION}/../benchmarks/exp_benchmarks/nmse_problem_3_4_2.fpcore  -e 1e2 1e1 1e0 1e-1 1e-2 1e-3 1e-4 1e-5 1e-6 1e-7 1e-8 | tee -a $date.html
-${FPTUNER} ${SCRIPT_LOCATION}/../benchmarks/exp_benchmarks/nmse_problem_3_4_4.fpcore  -e 1e2 1e1 1e0 1e-1 1e-2 1e-3 1e-4 1e-5 1e-6 1e-7 1e-8 1e-9 1e-10 1e-11 1e-12 1e-13 | tee -a $date.html
-${FPTUNER} ${SCRIPT_LOCATION}/../benchmarks/exp_benchmarks/nmse_section_3_11.fpcore  -e 1e2 1e1 1e0 1e-1 1e-2 1e-3 1e-4 1e-5 1e-6 1e-7 1e-8 1e-9 1e-10 1e-11 1e-12 1e-13 1e-14 | tee -a $date.html
-${FPTUNER} ${SCRIPT_LOCATION}/../benchmarks/exp_benchmarks/Statistics_Distribution_Poisson_Internal_probability_from_math_functions_0_1_5_2.fpcore  -e 1e2 1e1 1e0 1e-1 1e-2 1e-3 1e-4 1e-5 1e-6 1e-7 1e-8 1e-9 | tee -a $date.html
-
-cat <<EOF >> $date.html
+cat <<EOF >> $date/index.html
 </body>
 EOF
 
+mv ${SCRIPT_LOCATION}/../cost-measurement/*.png $date
+
 if [ "$(hostname)" = "warfa" ]; then
-    scp ${SCRIPT_LOCATION}/../cost-measurement/*.png $date.html uwplse.org:/var/www/fptuner/
+    scp -r $date uwplse.org:/var/www/fptuner/
 fi
